7980d8d9891e4e676205eb3ee1fa9340bf731380
everettraven <everettraven@gmail.com>
2025-06-20 17:56:35 -0400

    (feature): Add support for changing output format
    supporting both Markdown and JSON output formats.
    
    Signed-off-by: everettraven <everettraven@gmail.com>

---
+++ pkg/printers/markdown.go
@@ -0,0 +1,48 @@
+package printers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/everettraven/synkr/pkg/engine"
+	"github.com/everettraven/synkr/pkg/sources/github"
+)
+
+type Markdown struct{}
+
+func (md *Markdown) Print(result engine.SourceResult) (string, error) {
+	var out strings.Builder
+	out.WriteString(fmt.Sprintf("# %s - %s\n", result.Source, result.Project))
+
+	for _, item := range result.Items {
+		switch item := item.(type) {
+		case github.RepoItem:
+			out.WriteString(md.PrintGitHubRepoItem(item))
+		default:
+			out.WriteString(fmt.Sprintf("%v", item))
+		}
+
+		out.WriteString("\n")
+	}
+
+	return out.String(), nil
+}
+
+func (md *Markdown) PrintGitHubRepoItem(item github.RepoItem) string {
+	var out strings.Builder
+
+	out.WriteString(fmt.Sprintf("## [%s][%s]: %s \n", item.Type, item.State, item.Title))
+	out.WriteString(fmt.Sprintf("**URL**: %s\n", item.URL))
+	out.WriteString(fmt.Sprintf("**Author**: *%s*\n", item.Author))
+	out.WriteString(fmt.Sprintf("**Assignees**: %s\n", strings.Join(item.Assignees, ",")))
+
+	out.WriteString("\n")
+	labelStrs := []string{}
+	for _, label := range item.Labels {
+		labelStrs = append(labelStrs, fmt.Sprintf("`%s`", label))
+	}
+	out.WriteString(fmt.Sprintf("%s\n\n", strings.Join(labelStrs, " ")))
+	out.WriteString(fmt.Sprintf("%s\n", item.Body))
+
+	return out.String()
+}
